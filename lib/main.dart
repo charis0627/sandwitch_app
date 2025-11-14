e7cc2ae6d51a6882b0de6d5dc07df5241ae0a82e
charis0627 <charisva27@gmail.com>
2025-11-14 15:15:38 +0000

    add app_styles.dart file in lib

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Sandwich Shop App',
       home: OrderScreen(maxQuantity: 5),
     );
